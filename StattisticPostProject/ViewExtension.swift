4d2921e838277e68e8d15642b7afabda16d62c14
AndriiPp <pyvovarov.andrii@gmail.com>
2019-01-23 17:10:22 +0200

    add HTTPClient

---
+++ StattisticPostProject/ViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ViewExtension.swift
+//  StattisticPostProject
+//
+//  Created by Andrii Pyvovarov on 1/23/19.
+//  Copyright © 2019 Andrii Pyvovarov. All rights reserved.
+//
+
+import Foundation
